fd3c06fe5453af07c1dd8ae41dcdd3acdd9506b5
Your Name <you@example.com>
2018-09-25 16:39:48 +0530

    var1.sh

---
+++ bashscript/var1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+####writing a script to install a service,start it,enable it####
+
+package='firewalld'
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+sudo apt install $package
+
+#firewall installed##
+
